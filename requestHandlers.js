8962b9f62613703dc523b3cef1159084c41d89ec
Beth <embrown8@gmail.com>
2014-05-06 17:26:00 -0400

    Upload images and display them

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,16 +1,17 @@
 var querystring = require("querystring"),
-  fs = require("fs");
+  fs = require("fs"),
+  formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
   var body = '<html>' +
       '<head>' +
         '<meta http-equiv="Content-Type" content="text/html; ' +
         'charset=UTF-8" />' +
       '</head>' +
       '<body>' +
-        '<form action="upload" method="post">' +
-          '<textarea name="text" rows="20" cols="60"></textarea>' +
-          '<input type="submit" value="Submit text" />' +
+        '<form action="upload" enctype="multipart/form-data" method="post">' +
+          '<input type="file" name="upload">' +
+          '<input type="submit" value="Upload file" />' +
         '</form>' +
       '</body>' +
     '</html>';
@@ -21,11 +22,23 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent the text: " +
-    querystring.parse(postData).text);
-  response.end();
+function upload(response, request) {
+  var form = new formidable.IncomingForm();
+
+  form.parse(request, function(error, fields, files) {
+
+    fs.rename(files.upload.path, "/tmp/test.png", function(error){
+      if (error) {
+        fs.unlink("/tmp/test.png");
+        fs.rename(files.upload.path, "/tmp/test.png");
+      }
+    });
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Received image:<br/> ");
+    response.write("<img src='/show' />");
+  });
+
 }
 
 function show(response) {
